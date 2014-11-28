c4cf763ff57c69aaa9801cf08229cf8ce98f6450
Nicholas Zaharias <applzor@gmail.com>
2014-11-28 21:45:21 +1100

    Inital Commit

---
+++ SAT/Game1.cs
@@ -0,0 +1,117 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace SAT
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D circleTex, triangleTex;
+        Color colourA, colourB;
+        CircleCollider2D circleA, circleB;
+        TriangleCollider2D triangleA, triangleB;
+        Vector2 positionA, positionB;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            IsMouseVisible = true;
+            circleA = new CircleCollider2D(Vector2.Zero, 50.0f);
+            circleB = new CircleCollider2D(Vector2.Zero, 50.0f);
+            triangleA = new TriangleCollider2D(Vector2.Zero, new Vector2[3] { new Vector2(-50, -50), new Vector2(50, -50), new Vector2(50, 50) });
+            triangleB = new TriangleCollider2D(Vector2.Zero, new Vector2[3] { new Vector2(-50, -50), new Vector2(50, -50), new Vector2(50, 50) });
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            circleTex = Content.Load<Texture2D>("circle.png");
+            triangleTex = Content.Load<Texture2D>("triangle.png");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            positionA = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+            positionB = new Vector2(200);
+            if (circleA.Collision(positionA, triangleB, positionB))
+                if (triangleB.Collision(positionB, circleA, positionA))
+                    colourA = colourB = Color.Red;
+                else
+                    colourA = colourB = Color.White;
+            else
+                colourA = colourB = Color.White;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(circleTex, positionA - new Vector2(circleA.Radius), colourA);
+            spriteBatch.Draw(triangleTex, positionB - new Vector2(50), colourB);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SAT/Shape.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SAT
+{
+    abstract class Collider2D
+    {
+        protected Vector2 _Centre;
+        public Vector2 Centre { get { return _Centre; } }
+
+        protected Vector2[] _Points;
+        protected Vector2[] Points { get { return _Points; } }
+
+        public Collider2D(Vector2 Centre)
+        {
+            Centre = _Centre;
+        }
+
+        public abstract bool Collision(Vector2 Position, Collider2D Other, Vector2 OtherPosition);
+        public abstract Vector2 GetSATMinMax(Vector2 Axis, float Offset, Vector2 Position);
+    }
+
+    class CircleCollider2D : Collider2D
+    {
+        protected float _Radius;
+        public float Radius { get { return _Radius; } }
+
+        public CircleCollider2D(Vector2 Centre, float Radius)
+            : base(Centre)
+        {
+            _Radius = Radius;
+            _Points = new Vector2[1] { Vector2.Zero };
+        }
+
+        public override bool Collision(Vector2 Position, Collider2D Other, Vector2 OtherPosition)
+        {
+            Vector2 axis = OtherPosition - Position;
+            float offset = axis.Length();
+            axis.Normalize();
+            return CollisionSAT(axis, offset, Position, Other, OtherPosition);
+        }
+
+        protected bool CollisionSAT(Vector2 Axis, float Offset, Vector2 Position, Collider2D Other, Vector2 OtherPosition)
+        {
+            Vector2 minMaxA = GetSATMinMax(Axis, Offset, Position);
+            Vector2 minMaxB = Other.GetSATMinMax(Axis, Offset, OtherPosition);
+
+            //              minA    -   maxB
+            //              minB    -   maxA
+            float testA = minMaxA.X - minMaxB.Y;
+            float testB = minMaxB.X - minMaxA.Y;
+            if (testA > 0 || testB > 0)
+                return false;
+            return true;
+        }
+
+        public override Vector2 GetSATMinMax(Vector2 Axis, float Offset, Vector2 Position)
+        {
+            float point = Vector2.Dot(Axis, _Points[0] + Position + _Centre) + Offset;
+            return new Vector2(point - _Radius, point + _Radius);
+        }
+
+        void MinMax()
+        {
+            ////  Get A's min and max
+            //min1 = max1 = Vector2.Dot(axis, squareA.Points[0] + squareA.Positon);
+            //for (int i = 1; i < squareA.Points.Count; ++i)
+            //{
+            //    float dot = Vector2.Dot(axis, squareA.Points[i] + squareA.Positon);
+            //    if (dot < min1) min1 = dot;
+            //    if (dot > max1) max1 = dot;
+            //}
+            //
+            ////  Get B's min and max
+            //min2 = max2 = Vector2.Dot(axis, squareB.Points[0] + squareB.Positon);
+            //for (int i = 1; i < squareB.Points.Count; ++i)
+            //{
+            //    float dot = Vector2.Dot(axis, squareB.Points[i] + squareB.Positon);
+            //    if (dot < min2) min2 = dot;
+            //    if (dot > max2) max2 = dot;
+            //}
+            //
+            ////  Add on offset
+            //min1 += offset; max1 += offset;
+            //min2 += offset; max2 += offset;
+        }
+    }
+
+    class TriangleCollider2D : Collider2D
+    {
+        public TriangleCollider2D(Vector2 Centre, Vector2[] Points)
+            : base(Centre)
+        {
+            _Points = Points;
+        }
+
+        public override bool Collision(Vector2 Position, Collider2D Other, Vector2 OtherPosition)
+        {
+            for (int i = 0; i < _Points.Length; ++i)
+            {
+                Vector2 axis = _Points[i] - _Points[(i + 1) % _Points.Length];
+                float offset = axis.Length();
+                axis.Normalize();
+
+                float oldA = axis.X;
+                axis.X = axis.Y;
+                axis.Y = -oldA;
+                if (CollisionSAT(axis, offset, Position, Other, OtherPosition) == false)
+                    return false;
+            }
+            return true;
+        }
+
+        protected bool CollisionSAT(Vector2 Axis, float Offset, Vector2 Position, Collider2D Other, Vector2 OtherPosition)
+        {
+            Vector2 minMaxA = GetSATMinMax(Axis, Offset, Position);
+            Vector2 minMaxB = Other.GetSATMinMax(Axis, Offset, OtherPosition);
+
+            //              minA    -   maxB
+            //              minB    -   maxA
+            float testA = minMaxA.X - minMaxB.Y;
+            float testB = minMaxB.X - minMaxA.Y;
+            if (testA > 0 || testB > 0)
+                return false;
+            return true;
+        }
+
+        public override Vector2 GetSATMinMax(Vector2 Axis, float Offset, Vector2 Position)
+        {
+            float min, max;
+            min = max = Vector2.Dot(Axis, _Points[0] + Position + _Centre);
+            for (int i = 1; i < _Points.Length; ++i)
+            {
+                float dot = Vector2.Dot(Axis, _Points[i] + Position + _Centre);
+                if (dot < min) min = dot;
+                if (dot > max) max = dot;
+            }
+            return new Vector2(min + Offset, max + Offset);
+        }
+    }
+}
